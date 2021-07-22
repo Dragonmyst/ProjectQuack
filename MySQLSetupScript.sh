5956e12757233ac24360924c40b50b858ba76ff2
Michele-Punchcard <michele.thomas@punchcard.io>
2021-07-22 13:47:23 -0600

    creation of database initalization script

---
+++ MySQLSetupScript.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "Please enter root user MySQL password!"
+read -s rootpasswd
+
+mysql -u root -p${rootpasswd} -e "DROP DATABASE IF EXISTS projectquackdb;"
+echo ""
+
+echo "Creating database..."
+mysql -u root -p${rootpasswd} -e "CREATE DATABASE projectquackdb;"
+echo "Database created!"
+echo ""
+
+echo "Showing existing databases..."
+mysql -u root -p${rootpasswd} -e "SHOW DATABASES"
+echo ""
+
+echo "Creating table..."
+mysql -u root -p${rootpasswd} -e "USE projectquackdb; CREATE TABLE Feedings
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    datefed DATETIME NOT NULL,
+    createddate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    country VARCHAR(250) NOT NULL,
+    provincestate VARCHAR(250) NOT NULL,
+    citytown VARCHAR(250) NULL,
+    parkname VARCHAR(250) NULL,
+    duckcount INT NULL,
+    feedtype VARCHAR(250) NOT NULL,
+    quantity DOUBLE NULL,
+    quantityunit VARCHAR(250) NULL,
+    PRIMARY KEY pk (id)
+);"
+echo "Table created!"
+
+exit
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # ProjectQuack
+## About
+### Documentation
+- [Requirements Gathering]()
+- [Technology Choices]()
+- []()
+
+## Technology Stack
+- Frontend
+  - React
+- Backend
+  - Serverless using Azure Functions
+- Data Storage
+  - MySQL
+
+## How to setup
+### Tools Used
+- [Node.js](https://nodejs.org/en/)
+- [HomeBrew](https://brew.sh/)
+- [MySQL](https://dev.mysql.com/)
+
+### Steps to Run Locally:
+1. Start the MySQL server
+1. Run the following to initialize the database: 
+   %> ```bash MySQLSetupScript.sh```
+
+#### MySQL Commands
+- Start Server: $> ```brew services start mysql```
+- Stop Server: $> ```brew services stop mysql```
+- Connect to Server: $> ```mysql -u root -p```
