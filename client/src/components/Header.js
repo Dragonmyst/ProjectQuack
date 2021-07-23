ed915c746b0b3b1628bb3bb19cb477a8b37f92fb
Michele-Punchcard <michele.elizabeth.0103@gmail.com>
2021-07-23 14:22:18 -0600

    Added basic header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
 
 export const Header = () => {
-	return (
-		<React.Fragment>
-            <p>Coming Soon</p>
-        </React.Fragment>
-	)
-}
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
+            <Navbar.Brand href="/">
+                Project Quack
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="/about">About</Nav.Link>
+                    <Nav.Link href="/feederform">Feeder Form</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
