ebcc5e60be92e6ea8672fd2bbb7d235e10276aaf
Michele-Punchcard <michele.elizabeth.0103@gmail.com>
2021-07-26 19:34:07 -0600

    still trying to get function in cloud to connect to mysql in cloud

--- api/postFeedingData/index.js
+++ api/postFeedingData/index.js
@@ -6,25 +6,36 @@ module.exports = function (context, req) {
     console.log(process.env["MySQLConnection_HOST"]);
     context.log(process.env["MySQLConnection_HOST"]);
 
-    db.query(`
-        INSERT INTO Feedings 
-            (dateTimeFedUTC, country, provinceState, cityTown, parkName, duckCount, feedType, feedQty, feedQtyUnit) 
-        VALUES 
-            (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-        [data.datetimefed, data.country, data.provincestate, data.citytown, data.parkname, data.duckqty, data.feedtype, data.feedqty, data.feedqtyunit],
-        (error, results) => {
-            if (error) {
-                context.log("Error message was: " + error.message);
-                throw error;
-            } else {
-                context.res = {
-                    status: 201,
-                    body: {
-                        results
-                    }
-                };
+    db.connect(
+        function (err) {
+            if (err) {
+                console.log("!!! Cannot connect !!! Error:");
+                throw err;
+            }
+            else {
+                console.log("Connection established.");
 
-                context.done();
+                db.query(`
+                    INSERT INTO Feedings 
+                        (dateTimeFedUTC, country, provinceState, cityTown, parkName, duckCount, feedType, feedQty, feedQtyUnit) 
+                    VALUES 
+                        (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+                    [data.datetimefed, data.country, data.provincestate, data.citytown, data.parkname, data.duckqty, data.feedtype, data.feedqty, data.feedqtyunit],
+                    (error, results) => {
+                        if (error) {
+                            context.log("Error message was: " + error.message);
+                            throw error;
+                        } else {
+                            context.res = {
+                                status: 201,
+                                body: {
+                                    results
+                                }
+                            };
+
+                            context.done();
+                        }
+                    });
             }
         });
 };
